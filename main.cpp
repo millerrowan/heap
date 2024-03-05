e759fabeab661f95863e530c12472274b6366513
U-BSD\412170 <412170@SHS-95W2NW3.beaverton.k12.or.us>
2024-03-05 12:24:00 -0800

    commit 2

--- main.cpp
+++ main.cpp
@@ -7,7 +7,10 @@
    2/22/24
 */
 
-
+void add(int* heap[], int numCounter, int current);
+void print();
+void remove();
+void removeAll(); 
 
 int main() {
 
@@ -38,7 +41,7 @@ int main() {
 	fin.open(fileName);
 	int input1;
 	while(fin >> input) {
-	  add();
+	  add(heap, numCounter, input);
 	  numberCounter++;
 	}
 
@@ -46,13 +49,17 @@ int main() {
       fileName(strcmp(input, "MANUAL")==0) {
 	cout << "Type a number between 1 and 1000" << endl;
 	cin >> mannum;
-	numberCounter++;
-	add();
-	
+	add(heap, numCounter, mannum);
+	numCounter++; 
       }
 
     }
 
+    if(strcmp(input, "PRINT")==0) {
+
+
+    }
+    
     if(strcmp(input, "DELETE")==0) {
 
     }
@@ -71,14 +78,62 @@ int main() {
 }
 
 //heap value to the heap - adds to the available spot in array or tree then check to see if parent is larger than it, if parent is, swap with parent
-void add() {
-  
+void add(int* heap[], int numCounter, int current) {
+
+  //if there is nothing in the heap yet
+  if(numCounter == 0) {
+    heap[1] = current; 
+
+  }
+  //if there is stuff already in heap 
+  else() {
+      int currentIndex = numAdded+1;
+
+      heap[currentIndex] = current;
+
+      int parentIndex = floor(currentIndex/2);
+
+      while(parentIndex < currentIndex && currentIndex != 1) {
+	int temp = heap[parentIndex];
+	heap[parentIndex] = heap[currentIndex];
+	heap[currentIndex] = temp;
+
+	currentIndex = parentIndex;
+	parentIndex = floor(currentIndex/2); 
+      }
+   
+   }
   
 
 }
 
-void remove() {
+void print(int count, int* heap[], int index, int end) {
+  if((index*2) < end) {
+    print((index*2)+1, count+1, end);
+  }
+  for(int a = 0; a<count; a++) {
+    cout << "/t";
+    
+  }
+  cout << heap[index] << endl;
+  
+  if((index*2) < end) {
+    print((index*2), count+1, end); 
 
+  }
+
+}
+
+void remove(int* heap[], int numAdded) {
+  cout << heap[1] << endl; 
+  delete heap[1];
+  heap[1] = heap[numAdded];
+  numAdded-1;
+  int currentIndex = 1; 
+  int leftChild = (currentIndex*2);
+  int rightChild = ((currentIndex*2)+1); 
+  while(heap[1] <
+  
 
 }
 
